c80ffa1b45673db6bad60d058f8781b0a1d30b97
SlowlyKent <feliciakent31@gmail.com>
2024-08-24 21:36:30 +0800

    Add files via upload

--- MyFIrstAct_CPP.cpp
+++ MyFIrstAct_CPP.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 
 void listFile(){
-	int choice
+	int choice;
+	cout << "--------------------------\n";
 	cout << "\tLIST FILE DETAILS\n";
 	cout << "--------------------------\n";
 	cout << "1. List All Files \n";
@@ -21,19 +22,60 @@ void listOfAllFile(){
 }
 
 void createDirec(){
-	String direcName;
+	string direcName;
 	cout << "Enter the Directory name: \n";
 		cin >> direcName;
 	cout << "Current directory: C: \\Users\\UserName\\Documents\n";
 	cout << direcName << "Directory Successfully Created\n";
-	cout << "Press any key to continue\n";
+	cout << "Press any key to continue...\n\n";
+}
+ 
+ void changeDirec() {
+    string dirName;
+    int choice;
+    cout << "\nCurrent Directory: C:\\Users\\UserName\\Documents" << endl;
+    cout << "----------------------------\n";
+    cout << "Change Directory"<< endl;
+    cout << "----------------------------\n";
+    cout << "1. Step by Step Backward\n";
+    cout << "2. Goto Root Directory\n";
+    cout << "3. Forward Directory\n";
+    cout << "Enter the Number: ";
+    	cin >> choice;
+    cout <<""<<endl;
+    cout << "Please enter the Directory Name: ";
+    	cin >> dirName;
+    cout << "Current Directory: C:\\Users\\UserName\\Documents\\" << dirName << endl;
+    cout << "Press any key  to continue...\n\n";
 }
 
+void mainMenu() {
+    int choice;
+    do {
+    	cout << "-----------------------\n";
+        cout << "\tMAIN MENU\n";
+        cout << "-----------------------\n";
+        cout << "1. To Display List of Files\n";
+        cout << "2. To Create New Directory\n";
+        cout << "3. To Change the Working Directory\n";
+        cout << "4. Exit\n";
+        cout << "Enter the Number: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            listFile();
+            listOfAllFile();
+            break;
+        case 2:
+            
+		}
+
+
 int main (){
-	listFile();
 	
 
-	return 0:
+	return 0;
 }
 
 
